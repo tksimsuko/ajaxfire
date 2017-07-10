763972ebb44793c9a42b28f31dab035c8f6c9601
Takeshi Takashima <takashima-takeshi@ksk.co.jp>
2017-07-10 20:05:49 +0900

    update

--- ajaxfiresync.js
+++ ajaxfiresync.js
@@ -33,31 +33,20 @@ function AjaxFire(strategy, ajaxParam){
             },
             send: function(method, url, data, param){
                 if(!this.fire){
-                    this.fire = AsyncFire(strategy);
+                    this.fire = Fire(strategy);
                 }
-
-                var that = this;
                 this.fire.set(function(each, results, index){
-                    var sendData = data;
-                    if(typeof(data) === 'function'){
-                        sendData = data(results, index);//generate request data  from response data : series only
-                    }
-
                     ajax
-                        .send(method, url, sendData, param)
+                        .send(method, url, data, param)
                         .done(function(data, xhr){
                             results[index] = data;
                             each();
                         })
                         .fail(function(error, xhr){
-                            results[index] = {//TODO エラーに必要な項目の精査
+                            results[index] = {
                                 error: error,
                                 xhr: xhr
                             };
-
-                            if(that.fire.error(error, xhr, results, index)){//TODO parallel でもstopする
-                                return;
-                            }
                             each();
                         });
                 });
@@ -69,10 +58,7 @@ function AjaxFire(strategy, ajaxParam){
             },
             done: function(callback){
                 this.fire.onDone(callback);
-                return this;
-            },
-            error: function(callback){// callback return true -> stop series processing //TODO parallel でもstopする
-                this.fire.onError(callback);
+                this.fire.start();
                 return this;
             }
         }
@@ -215,12 +201,12 @@ function Ajax(defaultParam){
     }
 }
 /*
- * AsyncFire
+ * Fire
  * Copyright 2017 tksimsuko.
  * Licensed under the MIT License:
  * http://www.opensource.org/licenses/mit-license.php  
 */
-function AsyncFire(paramStrategy){
+function Fire(paramStrategy){
     var strategy = paramStrategy || 'parallel';
     return Flow(strategy);
     
@@ -233,13 +219,9 @@ function AsyncFire(paramStrategy){
         return {
             set: function(func){
                 if(!this.task){
-                    this.task = new Task();
-                    this.strategyFlow = generateStrategyFlow(flowStrategy);
+                    this.task = new Task()
                 }
-
                 this.task.funcs.push(func);
-                this.strategyFlow.execute(this);
-
                 return this;
             },
             onEach: function(callback){
@@ -250,8 +232,9 @@ function AsyncFire(paramStrategy){
                 this.done = callback;
                 return this;
             },
-            onError: function(callback){
-                this.error = callback;
+            start: function(){
+                this.strategyFlow = generateStrategyFlow(flowStrategy);
+                this.strategyFlow.start(this);
                 return this;
             }
         };
@@ -260,23 +243,19 @@ function AsyncFire(paramStrategy){
     //並行処理フロー
     // instance - flow, start, each, isDone
     function ParallelFlow(){
-        var current = 0;
-        var doneCount = 0;
+        var count = 0;
 
         return {
-            execute: function(flow){
-                if(!this.flow){
-                    this.flow = flow;
-                }
-
-                this.executeByIndex(current);
-                current++;
-            },
-            executeByIndex(index){
+            start: function(flow){//flow - Flow clousure
+                this.flow = flow;
+                
+                //処理実行
                 var that = this;
-                this.flow.task.funcs[index](function(){
-                    that.each(that.flow.task.results, index);
-                }, that.flow.task.results, index);
+                this.flow.task.funcs.forEach(function(func, index){
+                    func(function(){
+                        that.each(that.flow.task.results, index);
+                    }, that.flow.task.results, index);
+                });
             },
             each: function(results, index){
                 if(this.flow.each){
@@ -284,13 +263,13 @@ function AsyncFire(paramStrategy){
                 }
                 
                 //終了判定
-                doneCount++;
-                if(this.isDone() && this.flow.done){
+                count++;
+                if(this.isDone()){
                     this.flow.done(this.flow.task.results);
                 }
             },
             isDone: function(){
-                return doneCount === this.flow.task.funcs.length;
+                return count === this.flow.task.funcs.length;
             }
         };
     }
@@ -300,48 +279,32 @@ function AsyncFire(paramStrategy){
     // instance - flow, start, next, isDone
     function SeriesFlow(){
         var current = 0;
-        var doneCount = 0;
 
         return {
-            execute: function(flow){
-                if(!this.flow){
-                    this.flow = flow;
-                }
-                if(this.isWaiting()){
-                    return;
-                }
-
+            start: function(flow){//flow - Flow clousure
+                this.flow = flow;
                 this.next();
-            },
-            executeByIndex: function(index){
-                var that = this;
-                this.flow.task.funcs[index](function(){
-                    if(that.flow.each){
-                        that.flow.each(that.flow.task.results, index);
-                    }
-                    doneCount++;
-
-                    that.next();
-                }, that.flow.task.results, index);
             },
             next: function(){
                 //終了判定
                 if(this.isDone()){
-                    if(this.flow.done){
-                        this.flow.done(this.flow.task.results);
-                    }
+                    this.flow.done(this.flow.task.results);
                     return;
                 }
 
                 //処理実行
-                this.executeByIndex(current);
+                var that = this;
+                this.flow.task.funcs[current](function(){
+                    if(that.flow.each){
+                        that.flow.each(that.flow.task.results, current - 1);
+                    }
+                    that.next();
+                }, that.flow.task.results, current);
+
                 current++;
             },
-            isWaiting: function(){
-                return current != doneCount;
-            },
             isDone: function(){
-                return doneCount === this.flow.task.funcs.length;
+                return current === this.flow.task.funcs.length;
             }
         };
     }
---
+++ check.js
@@ -0,0 +1,49 @@
+var ajaxfire = AjaxFire('series', {responseType: 'document'});
+ajaxfire
+    .get('http://google.co.jp')
+    .get('http://yahoo.co.jp')
+    .get('https://news.google.com')
+    .get('https://www.pinterest.jp')
+    .get('https://vuejs.org')
+    .map(function(results, index){
+        console.log('1 ajaxfire map', results, index);
+    })
+    .error(function(error, xhr, results, index){
+        console.log('on error', error, xhr);
+        return true;
+    })
+    .done(function(results){
+        console.log('1 ajaxfire done', results);
+    });
+
+
+ajaxfire
+    .get('http://google.co.jp')
+    .get('http://yahoo.co.jp')
+    .post('http://localhost', function(results, index){
+        console.log('localhost post', results, index);
+        return results;
+    })
+    .get('https://news.google.com')
+    .get('https://www.pinterest.jp')
+    .get('https://vuejs.org')
+    .map(function(results, index){
+        console.log('1 ajaxfire map', results, index);
+    })
+    .error(function(error, xhr, results, index){
+        console.log('on error', error, xhr);
+        return true;
+    })
+    .done(function(results){
+        console.log('1 ajaxfire done', results);
+    });
+ajaxfire
+    .get('http://google.co.jp')
+    .get('http://yahoo.co.jp')
+    .get('https://news.google.com')
+    .map(function(results, index){
+        console.log('2 ajaxfire map', results, index);
+    })
+    .done(function(results){
+        console.log('2 ajaxfire done', results);
+    });
